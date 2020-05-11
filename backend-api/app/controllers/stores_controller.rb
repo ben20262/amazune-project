cae2ccd124afe88f2d19ee2dd53ef1a27940392a
ben20262 <ben20262@aim.com>
2020-05-11 14:24:27 -0400

    added stores controller

---
+++ backend-api/app/controllers/stores_controller.rb
@@ -0,0 +1,33 @@
+class StoresController < ApplicationController
+
+	def index
+		stores = Store.all
+		render json: stores
+	end
+
+	def show
+		store = Store.find_by(id: params[:id])
+		render json: store, include: :items
+	end
+
+	def create
+		store = Store.create(name: params[:name], content: params[:content], image: params[:image])
+		render json: store
+	end
+
+	def update
+		item = Item.create(name: params[:name], content: params[:content], image: params[:image])
+		render json: {type: 'SUCCESS'}
+	end
+
+	def destroy
+		store = Store.find_by(id: params[:id])
+		if store
+			Store.destroy(params[:id])
+			render json: {type: 'SUCCESS'}
+		else
+			render json: {type: 'FAILURE'}
+		end
+	end
+
+end
