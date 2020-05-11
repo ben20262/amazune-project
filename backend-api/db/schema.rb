cec61f106a8e6f436a389f787f44095092c46b00
ben20262 <ben20262@aim.com>
2020-05-11 14:33:47 -0400

    database migrated

---
+++ backend-api/db/schema.rb
@@ -0,0 +1,58 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_05_11_173455) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.float "price"
+    t.integer "store_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["store_id"], name: "index_items_on_store_id"
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.string "image"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_stores_on_user_id"
+  end
+
+  create_table "user_items", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "item_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_user_items_on_item_id"
+    t.index ["user_id"], name: "index_user_items_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "store_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["store_id"], name: "index_users_on_store_id"
+  end
+
+  add_foreign_key "items", "stores"
+  add_foreign_key "stores", "users"
+  add_foreign_key "user_items", "items"
+  add_foreign_key "user_items", "users"
+  add_foreign_key "users", "stores"
+end
--- backend-api/db/seeds.rb
+++ backend-api/db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: 'Ben')
+User.create(name: 'Tom')
+User.create(name: 'Jules')
+
+Store.create(name: 'Electronics', content: 'We sell electronics!', image: 'This is a picture', user_id: 1)
+Store.create(name: 'Toys', content: 'We sell toys!', image: 'This is a picture', user_id: 2)
+Store.create(name: 'Clothes', content: 'We sell clothes!', image: 'This is a picture', user_id: 3)
+
+Item.create(name: 'Television', content: 'High quality display.', price: 100, store_id: 1)
+Item.create(name: 'Hoverboard', content: 'Fun to ride!', price: 100, store_id: 2)
+Item.create(name: 'Shirt', content: 'Only the finest felts were used in this masterpiece.', price: 100, store_id: 3)
+
+UserItem.create(user_id: 1, item_id: 2, quantity: 1)
+UserItem.create(user_id: 2, item_id: 3, quantity: 2)
+UserItem.create(user_id: 3, item_id: 1, quantity: 3)
