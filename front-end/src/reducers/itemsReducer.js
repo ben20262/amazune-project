0152b2a2ad16b1ba2ada0745a0495e2bbfecc17c
ben20262 <ben20262@aim.com>
2020-05-12 14:35:09 -0400

    added swtich statement to items reducer

--- front-end/src/reducers/itemsReducer.js
+++ front-end/src/reducers/itemsReducer.js
@@ -1,4 +1,19 @@
 export default function rootReducer(state = {all: [], loading: false}, action) {
 
-	
+	switch (action.type) {
+		case 'LOADING_ITEMS':
+			return {
+				all: [...state.all],
+				loading: true
+			}
+		case 'ADD_ITEMS':
+			return {
+				all: action.items,
+				loading: false
+			}
+		case 'DELETE_ITEM':
+			return state
+		default:
+			return state
+	}
 }
