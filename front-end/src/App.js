583a5290039cb2dd791c3eceb5268b8d8d211adc
ben20262 <ben20262@aim.com>
2020-05-12 14:29:14 -0400

    added users and items containers

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux'
+import UsersContainer from './containers/UsersContainer'
+import ItemsContainer from './containers/ItemsContainer'
+import StoresContainer from './containers/StoreContainer'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Amazune.net</h1>
+				<UsersContainer />
+			</header>
+			<ItemsContainer />
+		</div>
+	);
 }
 
-export default App;
+export default connect(null, mapDispatchToProps)(App)
