7494cf2a7cfbf833932af25114611d861d95d01d
ben20262 <ben20262@aim.com>
2020-05-12 14:35:26 -0400

    added swtich statement to stores reducer

--- front-end/src/reducers/storesReducer.js
+++ front-end/src/reducers/storesReducer.js
@@ -1,4 +1,19 @@
 export default function storesReducer(state = {all: [], loading: false}, action) {
 
-	
+	switch (action.type) {
+		case 'LOADING_STORES':
+			return {
+				all: [...state.all],
+				loading: true
+			}
+		case 'ADD_STORES':
+			return {
+				all: action.stores,
+				loading: false
+			}
+		case 'DELETE_STORE':
+			return state
+		default:
+			return state
+	}
 }
