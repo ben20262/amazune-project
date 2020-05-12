4d0fd7faaa20644ee9eaad452396d11562b39050
ben20262 <ben20262@aim.com>
2020-05-12 19:06:24 -0400

    added number input for price

--- front-end/src/components/items/ItemInput.js
+++ front-end/src/components/items/ItemInput.js
@@ -5,7 +5,8 @@ export default class ItemInput extends Component {
 	state = {
 		name: '',
 		content: '',
-		image: ''
+		image: '',
+		price: '.00'
 	}
 	
 	handleChange = event => {
@@ -24,8 +25,9 @@ export default class ItemInput extends Component {
 		return (
 			<form onSubmit={this.handleSubmit} >
 				<input type='text' name='name' value={this.state.name} onChange={this.handleChange} />
-				<input type='text' name='content' value={this.state.content} onChange={this.handleChange} />
+				<textarea name='content' value={this.state.content} onChange={this.handleChange} />
 				<input type='text' name='image' value={this.state.image} onChange={this.handleChange} />
+				<input type='number' name='price' step='.01' value={this.state.price} onChange={this.handleChange} />
 				<input type='submit' />
 			</form>
 		)
