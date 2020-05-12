138332394339fbd7199a678779f3908f1b0e2a76
ben20262 <ben20262@aim.com>
2020-05-12 14:38:09 -0400

    added mapTo functions

---
+++ front-end/src/containers/StoresContainer.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchStores } from './actions/index'
+
+class StoresContainer extends Component {
+
+	componentDidMount() {
+		this.props.fetchStores()
+	}
+
+	render() {
+		return (
+			<div className='StoresContainer' >
+				
+			</div>
+		)
+	}
+}
+
+const mapStateToProps = state => {
+	return {
+		stores: state.stores
+	}
+}
+
+const mapDispatchToProps = dispatch => {
+	return {
+		fetchStores
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(StoresContainer)
