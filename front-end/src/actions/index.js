a97486c730fa24a6bfe957a873144dbf39de788e
ben20262 <ben20262@aim.com>
2020-05-12 14:56:42 -0400

    added create item action

--- front-end/src/actions/index.js
+++ front-end/src/actions/index.js
@@ -1,11 +1,13 @@
 const apiURL = 'http://localhost:3000'
 
-export const fetchUsers = () => {
-	return dispatch => {
-		dispatch({type: 'LOADING_USERS'})
-		fetch(`${apiURL}/users`)
-		.then(resp => resp.json())
-		.then(users => dispatch({ type: 'ADD_USERS', users }))
+const configObj = (action, info) => {
+	return {
+		method: action,
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		},
+		body: JSON.stringify(info)
 	}
 }
 
@@ -18,12 +20,30 @@ export const fetchItems = () => {
 	}
 }
 
+export const addItem = (name, content, price, storeId) => {
+	return dispatch => {
+		fetch(`${apiURL}/stores/${storeId}/items`, configObj('POST', { name, content, price }))
+		.then(resp => resp.json())
+		.then(item => dispatch({ type: 'CREATE_ITEM', item }))
+	}
+
+}
+
 export const fetchStores = () => {
 	return dispatch => {
 		dispatch({type: 'LOADING_STORES'})
 		fetch(`${apiURL}/stores`)
 		.then(resp => resp.json())
 		.then(stores => dispatch({ type: 'ADD_STORES', stores }))
+	}
+}
+
+export const fetchUsers = () => {
+	return dispatch => {
+		dispatch({type: 'LOADING_USERS'})
+		fetch(`${apiURL}/users`)
+		.then(resp => resp.json())
+		.then(users => dispatch({ type: 'ADD_USERS', users }))
 	}
 }
 
