063672c77ee6ed3ec3427650a5c5188e2cfc8165
ben20262 <ben20262@aim.com>
2020-05-12 14:35:40 -0400

    added swtich statement to users reducer

--- front-end/src/reducers/usersReducer.js
+++ front-end/src/reducers/usersReducer.js
@@ -1,4 +1,26 @@
 export default function usersReducer(state = {all: [], current: {}, loading: false}, action) {
 
-	
+	switch (action.type) {
+		case 'LOADING_USERS':
+			return {
+				...state,
+				all: [...state.all],
+				loading: true
+			}
+		case 'ADD_USERS':
+			return {
+				...state,
+				all: action.users,
+				loading: false
+			}
+		case 'LOGIN_USER':
+			return {
+				...state,
+				current: action.user
+			}
+		case 'CART_ITEM':
+			return state
+		default:
+			return state
+	}
 }
