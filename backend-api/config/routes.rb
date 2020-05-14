5c8062babcc3cbe2b66ae239a1b68ac2e0f2f0ab
ben20262 <ben20262@aim.com>
2020-05-14 17:52:01 -0400

    nested items in stores

--- backend-api/config/routes.rb
+++ backend-api/config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :index, :show, :update]
   resources :items, only: [:index, :create, :destroy]
   resources :stores, only: [:index, :show, :create, :update, :destroy]
+
+  resources :stores, only: [:show] do
+	resources :items, only: [:index, :create]
+  end
 end
