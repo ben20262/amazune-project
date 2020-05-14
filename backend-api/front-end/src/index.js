9bf73f6bf99352f8e83348dba40d19d8f10ad89e
ben20262 <ben20262@aim.com>
2020-05-14 11:56:20 -0400

    added thunk

--- backend-api/front-end/src/index.js
+++ backend-api/front-end/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import App from './App';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './reducers/index'
 
-let store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+let store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
 	<Provider store={store}>
