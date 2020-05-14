71236090e794b61b061abe02897e6ddd39e62bd6
ben20262 <ben20262@aim.com>
2020-05-14 11:31:16 -0400

    build built

---
+++ backend-api/front-end/build/precache-manifest.f3d1bc8935c1005e2460e62ba889ed19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b259af37e64bd84467b06194d9d5cafe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5d8d21943b3f1c59d4f",
+    "url": "/static/js/2.c4c0bb54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c4c0bb54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc61030e7c6c1565444",
+    "url": "/static/js/main.5d020e4f.chunk.js"
+  },
+  {
+    "revision": "55c0dd01550698e20362",
+    "url": "/static/js/runtime-main.ca362db2.js"
+  }
+]);
