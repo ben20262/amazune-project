18c6c8e6505fa5ad313902a1325d0aff15c0bc61
ben20262 <ben20262@aim.com>
2020-05-14 18:12:29 -0400

    added clear function

--- backend-api/front-end/src/components/items/ItemInput.js
+++ backend-api/front-end/src/components/items/ItemInput.js
@@ -11,23 +11,32 @@ export default class ItemInput extends Component {
 	
 	handleChange = event => {
 		this.setState({
-			...state,
 			[event.target.name]: event.target.value
 		})
 	}
 
 	handleSubmit = event => {
 		event.preventDefault()
-		this.props.addItem(this.state)
+		this.props.addItem(this.state.name, this.state.content, this.state.price, this.props.store_id)
+		this.setState({
+			name: '',
+			content: '',
+			image: '',
+			price: '.00'
+		})
 	}
 	
 	render() {
 		return (
 			<form onSubmit={this.handleSubmit} >
-				<input type='text' name='name' value={this.state.name} onChange={this.handleChange} />
-				<textarea name='content' value={this.state.content} onChange={this.handleChange} />
-				<input type='text' name='image' value={this.state.image} onChange={this.handleChange} />
-				<input type='number' name='price' step='.01' value={this.state.price} onChange={this.handleChange} />
+				<br />
+				<input type='text' name='name' value={this.state.name} onChange={this.handleChange} placeholder='Item Name' />
+				<br />
+				<textarea name='content' value={this.state.content} onChange={this.handleChange} placeholder='Description' />
+				<br />
+				<input type='text' name='image' value={this.state.image} onChange={this.handleChange} placeholder='Image' />
+				<br />
+				<input type='number' name='price' step='.01' value={this.state.price} onChange={this.handleChange} placeholder='Price' />
 				<input type='submit' />
 			</form>
 		)
