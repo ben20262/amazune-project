4f81e734682875d73d1af198e82d8a73c56c13e9
ben20262 <ben20262@aim.com>
2020-05-17 16:08:55 -0400

    added class name to user div

--- backend-api/front-end/src/components/users/User.js
+++ backend-api/front-end/src/components/users/User.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const User = props => {
 	return (
-		<div>
+		<div className='User-Component'>
 			<h4>{props.user.name}</h4>
 			<p>{props.user.items.length} Items In Cart</p>
 		</div>
