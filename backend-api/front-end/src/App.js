3bb9a596bf005aed1eedbed4e420769b856e2e93
ben20262 <ben20262@aim.com>
2020-05-17 16:00:50 -0400

    added routes and nav bar

--- backend-api/front-end/src/App.js
+++ backend-api/front-end/src/App.js
@@ -1,18 +1,43 @@
 import React from 'react';
+import { BrowserRouter as Router,
+	Route,
+	Switch,
+	Link
+	} from 'react-router-dom'
 import UsersContainer from './containers/UsersContainer'
 import ItemsContainer from './containers/ItemsContainer'
 import StoresContainer from './containers/StoresContainer'
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>Amazune.net</h1>
-				<UsersContainer />
-			</header>
-			<ItemsContainer />
-			<StoresContainer />
-		</div>
+		<Router>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to='/'>Home</Link>
+						</li>
+						<li>
+							<Link to='/items'>Items</Link>
+						</li>
+						<li>
+							<Link to='/stores'>Stores</Link>
+						</li>
+					</ul>
+				</nav>
+
+				<Switch>
+					<Route path='/items'>
+						<ItemsContainer />
+					</Route>
+					<Route path='/stores' render={routerProps => <StoresContainer {...routerProps} />} />
+					<Route path='/'>
+						<UsersContainer />
+					</Route>
+					<Route component={() => <UsersContainer />} />
+				</Switch>
+			</div>
+		</Router>
 	);
 }
 
