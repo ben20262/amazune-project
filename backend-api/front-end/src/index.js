22445e10088a9711b8f5efcc0aa992a1068574af
ben20262 <ben20262@aim.com>
2020-05-26 13:54:28 -0400

    imported compose and added devtools to store

--- backend-api/front-end/src/index.js
+++ backend-api/front-end/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './reducers/index'
 import './index.css'
 
-let store = createStore(rootReducer, applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+))
 
 ReactDOM.render(
 	<Provider store={store}>
